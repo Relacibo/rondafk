59a46f1b6cbd132aa859d8e88cf60130276a8a58
Reinhard Bronner <reinhard_bronner@t-online.de>
2025-09-15 02:35:58 +0200

    trash

--- src/lib.rs
+++ src/lib.rs
@@ -20,20 +20,23 @@ pub fn format(
     extentions
         .iter()
         .try_for_each(|e| write!(buf, "{}", extention(e, c)))
-        .expect("neither <String as fmt::Write> nor ron_edit's Display implementations error");
+        .inspect_err(|err| eprintln!("{err}"))
+        .ok();
     if extentions.is_empty() {
-        write!(buf, "{}", ws_lead_min(value, c, &crate::value))
+        ws_lead_min(buf, value, c, &format_value)
+            .inspect_err(|err| eprintln!("{err}"))
+            .ok();
     } else {
-        write!(
-            buf,
-            "{}{}",
-            ws_lead_nl(value, c, &crate::value),
-            ws_min(trailing_ws, c)
-        )
-    }
-    .expect("neither <String as fmt::Write> nor ron_edit's Display implementations error");
-    write!(buf, "{}", ws_general(trailing_ws, c, c, true, false))
-        .expect("neither <String as fmt::Write> nor ron_edit's Display implementations error");
+        ws_lead_nl(buf, value, c, &format_value)
+            .inspect_err(|err| eprintln!("{err}"))
+            .ok();
+        format_ws_min(buf, trailing_ws, c)
+            .inspect_err(|err| eprintln!("{err}"))
+            .ok();
+    };
+    format_ws(buf, trailing_ws, c, c, false, true, false)
+        .inspect_err(|err| eprintln!("{err}"))
+        .ok();
     out
 }
 
@@ -60,7 +63,7 @@ impl Context {
     }
 }
 
-fn value<'a>(it: &'a Value, c @ Context { nl, .. }: Context) -> impl Display + 'a {
+fn format_value<'a>(it: &'a Value, c @ Context { nl, .. }: Context) -> impl Display + 'a {
     Disp(move |f| match it {
         lit @ (Value::Int(_)
         | Value::Float(_)
@@ -80,61 +83,55 @@ fn value<'a>(it: &'a Value, c @ Context { nl, .. }: Context) -> impl Display + '
             }
         ),
         Value::List(List(list)) => {
-            write!(f, "[{}]", separated_split(list, &crate::value, c))
+            write!(f, "[")?;
+            format_separated(f, list, c, &format_value, false)?;
+            write!(f, "]")
         }
         Value::Map(Map(fields)) => {
-            write!(
-                f,
-                "{{{}}}",
-                separated_split(
-                    fields,
-                    &|MapItem {
-                          key,
-                          after_key,
-                          value,
-                      },
-                      c| Disp(move |f| write!(
-                        f,
-                        "{key}{}:{}",
-                        ws_min(after_key, c),
-                        ws_lead_single(value, c, &crate::value)
-                    )),
-                    c
-                )
-            )
+            write!(f, "{{")?;
+            format_separated(f, fields, c, &format_map_item, false)?;
+            write!(f, "}}")
         }
-        Value::Tuple(Tuple { ident, fields }) => write!(
-            f,
-            "{}({})",
-            option(ident, |ident| ws_followed_min(ident, c, &|s, _| s)),
-            if fields.values.len() > 1 {
-                separated_split(fields, &crate::value, c).to_string()
-            } else {
-                separated_min(fields, c, &crate::value).to_string()
-            }
-        ),
+        Value::Tuple(Tuple { ident, fields }) => {
+            option(f, ident, |ident| ws_followed_min(ident, c, &|s, _| s))?;
+            write!(f, "(",)?;
+            format_separated(f, fields, c, &format_value, fields.values.len() <= 1)?;
+            write!(f, ")")
+        }
         Value::Struct(Struct { ident, fields }) => {
-            write!(
-                f,
-                "{}({})",
-                option(ident, |ident| ws_followed_min(ident, c, &|s, _| s)),
-                separated_split(
-                    fields,
-                    &|NamedField {
-                          key,
-                          after_key,
-                          value,
-                      },
-                      c| Disp(move |f| write!(
-                        f,
-                        "{key}{}:{}",
-                        ws_min(after_key, c),
-                        ws_lead_single(value, c, &crate::value),
-                    )),
-                    c
-                )
-            )
+            option(f, ident, |ident| ws_followed_min(ident, c, &|s, _| s))?;
+            write!(f, "(")?;
+            format_separated(f, fields, c, &format_named_field, false)?;
+            write!(f, "(")
         }
+    })
+}
+
+fn format_map_item<'a>(map_item: &'a MapItem<'_>, c: Context) -> impl Display + 'a {
+    let MapItem {
+        key,
+        after_key,
+        value,
+    } = map_item;
+    Disp(move |f| {
+        write!(f, "{key}",)?;
+        format_ws_min(f, after_key, c)?;
+        write!(f, ":")?;
+        ws_lead_single(f, value, c, &format_value)
+    })
+}
+
+fn format_named_field<'a>(named_field: &'a NamedField<'_>, c: Context) -> impl Display + 'a {
+    Disp(move |f| {
+        let NamedField {
+            key,
+            after_key,
+            value,
+        } = named_field;
+        write!(f, "{key}",)?;
+        format_ws_min(f, after_key, c)?;
+        write!(f, ":")?;
+        ws_lead_single(f, value, c, &format_value)
     })
 }
 
@@ -168,17 +165,21 @@ fn extention<'a>(
     c: Context,
 ) -> impl Display + 'a {
     Disp(move |f| {
-        write!(
-            f,
-            "{}#{}!{}[{}enable{}({}){}]",
-            ws_min(leading, c),
-            ws_min(after_pound, c),
-            ws_min(after_exclamation, c),
-            ws_min(after_bracket, c),
-            ws_min(after_enable, c),
-            separated_min(extentions, c, &|&s, _| s),
-            ws_min(after_paren, c),
-        )
+        format_ws_min(f, leading, c)?;
+        write!(f, "#")?;
+        format_ws_min(f, after_pound, c)?;
+        write!(f, "!")?;
+        format_ws_min(f, after_exclamation, c)?;
+        write!(f, "[")?;
+        format_ws_min(f, after_bracket, c)?;
+        write!(f, "enable")?;
+        format_ws_min(f, after_enable, c)?;
+        write!(f, "(")?;
+        format_separated(f, extentions, c, &|&s, _| s, true)?;
+        write!(f, ")")?;
+        format_ws_min(f, after_paren, c)?;
+        write!(f, "]")?;
+        Ok(())
     })
 }
 
@@ -224,7 +225,7 @@ fn space_helpers() {
 }
 
 fn ws_inner(
-    f: &mut Formatter,
+    f: &mut impl Write,
     ws: &Ws,
     c_after: Context,
     is_standalone: bool,
@@ -256,161 +257,165 @@ fn ws_inner(
     Ok(res)
 }
 
-fn ws_min<'a>(ws: &'a Whitespace<'_>, c: Context) -> impl Display + 'a {
-    ws_general(ws, c, c, false, false)
-}
-fn ws_single<'a>(ws: &'a Whitespace<'_>, c: Context) -> impl Display + 'a {
-    ws_general(ws, c, c, false, true)
+fn format_ws_min(
+    f: &mut impl Write,
+    ws: &'_ Whitespace<'_>,
+    c: Context,
+) -> Result<bool, fmt::Error> {
+    format_ws(f, ws, c, c, false, false, false)
 }
 
-fn ws_general<'a>(
+fn format_ws_single<'a>(
+    f: &mut impl Write,
+    ws: &'a Whitespace<'_>,
+    c: Context,
+) -> Result<bool, fmt::Error> {
+    format_ws(f, ws, c, c, false, false, true)
+}
+
+fn format_ws<'a>(
+    f: &mut impl Write,
     Whitespace(ws): &'a Whitespace,
     c @ Context { indent, nl }: Context,
     c_after @ Context {
         indent: indent_after,
         nl: nl_after,
     }: Context,
-    add_trailing_nl: bool,
+    mut is_standalone: bool,
+    always_add_trailing_nl: bool,
     pad_end_with_space: bool,
-) -> impl Display + 'a {
-    Disp(move |f| {
-        // Search for last non_space Ws
-        let last_elem_position = ws
-            .iter()
-            .rev()
-            .position(|elem| !matches!(elem, Ws::Space(_)));
-        let after_last_elem_position = last_elem_position.map(|l| ws.len() - l).unwrap_or_default();
-        let mut is_standalone = false;
-        let mut observed_newline = false;
-        for (i, elem) in ws[..after_last_elem_position].iter().enumerate() {
-            // Preserve, whether comment is standalone or preceded by non whitespace
-            match elem {
-                Ws::Space(s) if !observed_newline => {
-                    if s.contains("\n") {
-                        observed_newline = true;
-                    }
+) -> Result<bool, fmt::Error> {
+    // Search for last non_space Ws
+    let last_elem_position = ws
+        .iter()
+        .rev()
+        .position(|elem| !matches!(elem, Ws::Space(_)));
+    let after_last_elem_position = last_elem_position.map(|l| ws.len() - l).unwrap_or_default();
+    let mut observed_newline = false;
+    for (i, elem) in ws[..after_last_elem_position].iter().enumerate() {
+        // Preserve, whether comment is standalone or preceded by non whitespace
+        match elem {
+            Ws::Space(s) if !observed_newline => {
+                if s.contains("\n") {
+                    observed_newline = true;
                 }
-                Ws::LineComment(_) | Ws::BlockComment(_) => {
-                    if observed_newline && !is_standalone {
-                        write!(f, "{nl}{indent}")?;
-                        is_standalone = true;
-                    }
-                    let c = if i == after_last_elem_position - 1 {
-                        c_after
-                    } else {
-                        c
-                    };
-                    is_standalone = ws_inner(f, elem, c, is_standalone)?;
-                    observed_newline = false;
-                }
-                _ => {}
             }
+            Ws::LineComment(_) | Ws::BlockComment(_) => {
+                if observed_newline && !is_standalone {
+                    write!(f, "{nl}{indent}")?;
+                    is_standalone = true;
+                }
+                let c = if i == after_last_elem_position - 1 {
+                    c_after
+                } else {
+                    c
+                };
+                is_standalone = ws_inner(f, elem, c, is_standalone)?;
+                observed_newline = false;
+            }
+            _ => {}
         }
+    }
 
-        if !is_standalone {
-            if add_trailing_nl {
-                write!(f, "{nl_after}{indent_after}")?;
-            } else if pad_end_with_space {
-                write!(f, " ")?;
-            }
+    if !is_standalone {
+        if always_add_trailing_nl {
+            write!(f, "{nl_after}{indent_after}")?;
+        } else if pad_end_with_space {
+            write!(f, " ")?;
         }
-        Ok(())
-    })
+    }
+    Ok(is_standalone)
 }
 
-fn separated_min<'a, T, D: Display>(
+fn format_separated<'a, T, D: Display>(
+    f: &mut impl Write,
     Separated {
         values,
         trailing_comma: _,
         trailing_ws,
     }: &'a Separated<'a, T>,
     c: Context,
-    item: &'a impl Fn(&'a T, Context) -> D,
-) -> impl Display + 'a {
-    Disp(move |f| {
-        for (i, elem) in values.iter().enumerate() {
-            let pad_end_with_space = i != 0;
-            // write!(f, ",")?;
-            write!(
-                f,
-                "{}{},{}",
-                ws_general(&elem.leading, c, c, false, pad_end_with_space),
-                item(&elem.content, c),
-                ws_min(&elem.following, c)
-            )?;
-        }
-        write!(f, "{}", ws_min(trailing_ws, c))
-    })
-}
-
-fn separated_split<'a, T, D: Display>(
-    Separated {
-        values,
-        trailing_comma: _,
-        trailing_ws,
-    }: &'a Separated<'a, T>,
-    item: &'a impl Fn(&'a T, Context) -> D,
-    c: Context,
-) -> impl Display + 'a {
-    Disp(move |f| {
-        let increased = c.increase();
-        for elem in values {
-            write!(
-                f,
-                "{}{},{}",
-                ws_general(&elem.leading, c, c, true, false),
-                item(&elem.content, c),
-                ws_min(&elem.following, c)
-            )?;
-        }
-        write!(f, "{}", ws_general(trailing_ws, increased, c, true, false))
-    })
+    format_content: &'a impl Fn(&'a T, Context) -> D,
+    minimal: bool,
+) -> fmt::Result {
+    let increased = c.increase();
+    let mut is_standalone = false;
+    for (i, elem) in values.iter().enumerate() {
+        let leading_always_add_trailing_newline = !minimal;
+        let leading_pad_end_with_space = minimal && i != 0;
+        let following_always_add_trailing_newline = i == values.len();
+        let c_after = if i == values.len() { c } else { increased };
+        format_ws(
+            f,
+            &elem.leading,
+            increased,
+            increased,
+            is_standalone,
+            leading_always_add_trailing_newline,
+            leading_pad_end_with_space,
+        )?;
+        write!(f, "{},", format_content(&elem.content, increased),)?;
+        is_standalone = format_ws(
+            f,
+            &elem.following,
+            increased,
+            c_after,
+            false,
+            following_always_add_trailing_newline,
+            false,
+        )?;
+    }
+    format_ws(f, trailing_ws, increased, c, is_standalone, !minimal, false)?;
+    Ok(())
 }
 
 fn ws_lead_min<'a, T, D: Display>(
+    f: &mut impl Write,
     WsLead { leading, content }: &'a WsLead<T>,
     c: Context,
     format_content: &'a impl Fn(&'a T, Context) -> D,
-) -> impl Display + 'a {
-    Disp(move |f| write!(f, "{}{}", ws_min(leading, c), format_content(content, c)))
+) -> fmt::Result {
+    format_ws_min(f, leading, c)?;
+    write!(f, "{}", format_content(content, c))
 }
 fn ws_followed_min<'a, T, D: Display>(
     WsFollowed { content, following }: &'a WsFollowed<T>,
     c: Context,
     format_content: &'a impl Fn(&'a T, Context) -> D,
 ) -> impl Display + 'a {
-    Disp(move |f| write!(f, "{}{}", format_content(content, c), ws_min(following, c)))
+    Disp(move |f| {
+        write!(f, "{}", format_content(content, c),)?;
+        format_ws_min(f, following, c)?;
+        Ok(())
+    })
 }
 fn ws_lead_single<'a, T, D: Display>(
+    f: &mut impl Write,
     WsLead { leading, content }: &'a WsLead<T>,
     c: Context,
     format_content: &'a impl Fn(&'a T, Context) -> D,
-) -> impl Display + 'a {
-    Disp(move |f| write!(f, "{}{}", ws_single(leading, c), format_content(content, c)))
+) -> fmt::Result {
+    format_ws_single(f, leading, c)?;
+    write!(f, "{}", format_content(content, c))?;
+    Ok(())
 }
 fn ws_lead_nl<'a, T, D: Display>(
+    f: &mut impl Write,
     WsLead { leading, content }: &'a WsLead<T>,
     c: Context,
     format_content: &'a impl Fn(&'a T, Context) -> D,
-) -> impl Display + 'a {
-    Disp(move |f| {
-        write!(
-            f,
-            "{}{}",
-            ws_general(leading, c, c, true, false),
-            format_content(content, c)
-        )
-    })
+) -> fmt::Result {
+    format_ws(f, leading, c, c, false, true, false)?;
+    write!(f, "{}", format_content(content, c))
 }
 fn option<'a, T, D: Display>(
+    f: &mut impl Write,
     opt: &'a Option<T>,
     format_content: impl Fn(&'a T) -> D + 'a,
-) -> impl Display + 'a {
-    Disp(move |f| {
-        if let Some(value) = opt {
-            write!(f, "{}", format_content(value))
-        } else {
-            Ok(())
-        }
-    })
+) -> fmt::Result {
+    if let Some(value) = opt {
+        write!(f, "{}", format_content(value))
+    } else {
+        Ok(())
+    }
 }
